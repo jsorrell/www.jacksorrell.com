694712631415458c19cb38d22ca0df8f03605b3b
Jack Sorrell <jack@jacksorrell.com>
2018-01-23 02:49:19 -0500

    Fix popup behavior on clicks

--- public/javascripts/resume.js
+++ public/javascripts/resume.js
@@ -1,11 +1,16 @@
 $(function () {
-  $('[data-toggle="popover"]').popover()
+	$('[data-toggle="popover"]').popover()
+	
+	$(document).on('click', function (e) {
+		$('[data-toggle="popover"],[data-original-title]').each(function () {
+			if (!$(this).is(e.target) && $('.popover').has(e.target).length === 0) {                
+				(($(this).popover('hide').data('bs.popover')||{}).inState||{}).click = false
+			}
+		});
+	});
+
+	$('a.section-row-toggle').click(function(){
+		$(this).find('.dropdown-arrow').toggleClass('dropdown-arrow-flipped');
+	});
 })
 
-$('.popover-dismiss').popover({
-  trigger: 'focus'
-})
-
-$('a.section-row-toggle').click(function(){
-	$(this).find('.dropdown-arrow').toggleClass('dropdown-arrow-flipped');
-});
