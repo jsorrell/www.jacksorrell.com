bf7e51a760803fc5c6121389c30d456117282dd3
Jack Sorrell <jack@jacksorrell.com>
2018-02-03 02:43:08 -0500

    Fix popovers on courses

--- public/javascripts/resume.js
+++ public/javascripts/resume.js
@@ -3,7 +3,7 @@ $(function () {
 	
 	$(document).on('click', function (e) {
 		$('[data-toggle="popover"],[data-original-title]').each(function () {
-			if (!$(this).is(e.target) && $('.popover').has(e.target).length === 0) {                
+			if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.popover').has(e.target).length === 0) {                
 				(($(this).popover('hide').data('bs.popover')||{}).inState||{}).click = false
 			}
 		});
