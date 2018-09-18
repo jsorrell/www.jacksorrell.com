be3857e4b490738dc17aa29adca74b07240b9b7b
Jack Sorrell <Jack@jacksorrell.com>
2018-09-18 15:17:58 -0400

    Generate favicons

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -6,6 +6,7 @@ var autoprefixer = require('autoprefixer');
 var browserSync = require('browser-sync').create();
 var browserify = require('browserify');
 var spawn = require('child_process').spawn;
+var favicons = require('favicons').stream;
 var gulp = require('gulp');
 const gulpClean = require('gulp-clean');
 var minifycss = require('gulp-clean-css');
@@ -29,6 +30,40 @@ const yaml = require('js-yaml');
 const fs = require('fs');
 const argv = require('yargs').argv;
 
+const htmlMinConfig = { collapseWhitespace: true };
+
+const faviconConfiguration = {
+	path: '/fav/',
+	appName: 'www.jacksorrell.com',
+	appDescription: 'The personal website of Jack Sorrell',
+	developerName: 'Jack Sorrell',
+	developerURL: 'https://www.jacksorrelll.com/',
+	dir: 'auto',
+	lang: 'en-US',
+	background: '#66648b',
+	theme_color: '#66648b',
+	appleStatusBarStyle: 'black-translucent',
+	display: 'standalone',
+	orientation: 'portrait',
+	scope: '/',
+	start_url: '/',
+	version: '1.0',
+	logging: true,
+	icons: {
+		android: true,
+		appleIcon: true,
+		appleStartup: true,
+		coast: true,
+		favicons: true,
+		firefox: true,
+		windows: true,
+		yandex: false
+	},
+	replace: true,
+	html: 'out.html',
+	pipeHTML: true
+};
+
 var serverPort = argv.port;
 
 if (serverPort === undefined) {
@@ -45,7 +80,7 @@ var bsPort = (argv.bsport === undefined) ? serverPort + 1 : Number(argv.bsport);
 const sass = { src: path.join(__dirname, 'client/sass/'), dest: path.join(__dirname, 'assets/public/css/') };
 const goHtml = { src: path.join(__dirname, 'client/views/'), dest: path.join(__dirname, 'assets/templates/') };
 const ts = { src: path.join(__dirname, 'client/scripts/'), dest: path.join(__dirname, 'assets/public/js/') };
-const favicon = { src: path.join(__dirname, 'client/favicon/public/'), dest: path.join(__dirname, 'assets/public/fav/') };
+const favicon = { src: path.join(__dirname, 'client/favicon/favicon.svg'), htmlFile: 'assets/templates/includes/favicon.gohtml', favPrefix: 'assets/public/fav/' };
 
 var clientTs = typescript.createProject(path.join(ts.src, 'tsconfig.json'));
 
@@ -85,23 +120,32 @@ export function compileTs () {
 	.pipe(gulp.dest(ts.dest));
 }
 
-// TODO generate favicons
-export function copyFavicons () {
-	return gulp.src(path.join(favicon.src, '**'))
-		.pipe(newer(favicon.dest))
-		.pipe(print(filepath => `copied: ${filepath}`))
-		.pipe(gulp.dest(favicon.dest));
+export function genFavicons (done) {
+	return gulp.src(favicon.src)
+		.pipe(newer({ map: function (_) {
+			return path.join(favicon.favPrefix, 'favicon.ico');
+		}}))
+		.pipe(favicons(faviconConfiguration))
+		.pipe(tap(function (file, t) {
+			if (path.extname(file.path) === '.html') {
+				file.path = favicon.htmlFile;
+				t.through(htmlmin, [htmlMinConfig]);
+			} else {
+				file.path = path.join(favicon.favPrefix, file.path);
+			}
+		}))
+		.pipe(gulp.dest(__dirname));
 }
 
 export function copyViews () {
 	return gulp.src(path.join(goHtml.src, '**/*.gohtml'))
 		.pipe(newer(goHtml.dest))
-		.pipe(htmlmin({ collapseWhitespace: true }))
+		.pipe(htmlmin(htmlMinConfig))
 		.pipe(print(filepath => `copied: ${filepath}`))
 		.pipe(gulp.dest(goHtml.dest));
 }
 
-export const build = gulp.parallel(compileSass, compileTs, copyFavicons, copyViews);
+export const build = gulp.parallel(compileSass, compileTs, genFavicons, copyViews);
 
 function bsInit (done) {
 	browserSync.init({
@@ -155,22 +199,22 @@ export function lintGo () {
 export const lint = gulp.series(lintJs, lintTs, lintGo);
 
 export function cleanTemplates () {
-	return gulp.src(goHtml.dest, { read: false })
+	return gulp.src(goHtml.dest, { read: false, allowEmpty: true })
 		.pipe(gulpClean());
 }
 
 export function cleanCss () {
-	return gulp.src(sass.dest, { read: false })
+	return gulp.src(sass.dest, { read: false, allowEmpty: true })
 		.pipe(gulpClean());
 }
 
 export function cleanJs () {
-	return gulp.src(ts.dest, { read: false })
+	return gulp.src(ts.dest, { read: false, allowEmpty: true })
 		.pipe(gulpClean());
 }
 
 export function cleanFavicons () {
-	return gulp.src(favicon.dest, { read: false })
+	return gulp.src(path.join(__dirname, favicon.favPrefix), { read: false, allowEmpty: true })
 		.pipe(gulpClean());
 }
 
