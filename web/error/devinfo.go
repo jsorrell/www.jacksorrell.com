298c597645ebf8a401c4ffc88b7af5eb98feb5aa
Jack Sorrell <Jack@jacksorrell.com>
2018-09-20 03:22:34 -0400

    Comment DevInfo

--- web/error/devinfo.go
+++ web/error/devinfo.go
@@ -1,5 +1,6 @@
 package error
 
+// DevInfo contains development information that is passed to error handlers in development mode only.
 type DevInfo struct {
 	stacktrace []byte
 	file       string
